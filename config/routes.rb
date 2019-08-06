e313536c64ee4a1dbbcfddf44f80636063ce867d
Osazeme <osazeme@omise.co>
2019-08-06 15:30:22 +0100

    added rake-cors and respective routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :files, only: :create
+    get "files/:tag_search_query/:page", to: "files#show", as: "file"
+  end
 end
