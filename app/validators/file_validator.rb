133a313433ef9773f3f018901391c3104e1222f6
Osazeme <osazeme@omise.co>
2019-08-09 09:46:09 +0100

    implemented default error routes

--- app/validators/file_validator.rb
+++ app/validators/file_validator.rb
@@ -8,10 +8,9 @@ class FileValidator < ActiveModel::EachValidator
   MAX_SIZE = 100000
 
   def validate_each(record, attribute, value)
-    if value.attached?
-      record.errors.add(attribute, "has wrong format") unless value.attachment.blob.content_type.in?(PERMITTED_CONTENT_TYPES)
-      record.errors.add(attribute, "has invalid filesize") unless value.blob.byte_size < MAX_SIZE
-      value.purge_later unless record.errors.messages.blank?
-    end
+    errors.add(:base, 'Please upload valid document') unless value.attached?
+    record.errors.add(attribute, "has wrong format") unless value.attachment.blob.content_type.in?(PERMITTED_CONTENT_TYPES)
+    record.errors.add(attribute, "has invalid filesize") unless value.blob.byte_size < MAX_SIZE
+    value.purge_later unless record.errors.messages.blank?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
+    get '/404', to: 'errors#not_found'
+    get '/500', to: 'errors#internal_server_error'
+
     resources :files, only: :create
     get "files/:tag_search_query/:page", to: "files#show", as: "file"
   end
